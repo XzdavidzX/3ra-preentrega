3e37c26d609cf95a7bd2754e4a5dfcc7a26cf81c
XzdavidzX <davidh.dg76@gmail.com>
2024-02-08 16:39:07 -0300

    3ra pre-entrega

--- proyecto/aplicacion/models.py
+++ proyecto/aplicacion/models.py
@@ -30,10 +30,3 @@ class Profesor(models.Model):
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
     
-class Entregable(models.Model):
-    nombre = models.CharField(max_length=50)
-    fechaEntrega = models.DateField()
-    entregado = models.BooleanField()
-
-    def __str__(self):
-        return f"{self.nombre}"
